b9e1982602aaa85dddf807dedb3cc67cfa9640b6
Felipe Cadar <cadar@dcc.ufmg.br>
2024-04-15 15:37:19 +0200

    remove pup;licates

---
+++ scripts/remove_duplicates.py
@@ -0,0 +1,28 @@
+import os
+import json
+from tqdm import tqdm
+
+DB_PATH = 'public/papers_info.json'
+
+DB = json.load(open(DB_PATH))
+
+print(f'Loaded {len(DB)} papers')
+abstract_max_len = 10_000
+
+# remove duplicates by 'title'
+abtract_lens = []
+unique_papers = {}
+for paper in tqdm(DB):
+    title = paper['title']
+    # limt abstract length
+    paper['abstract'] = paper['abstract'][:abstract_max_len]
+    if title not in unique_papers:
+        unique_papers[title] = paper
+    else:
+        # check if has more keys
+        if len(paper.keys()) > len(unique_papers[title].keys()):
+            unique_papers[title] = paper
+            print(f"Replaced: {title} with {paper.keys()} keys by {unique_papers[title].keys()} keys") 
+            
+new_db = list(unique_papers.values())
+json.dump(new_db, open(DB_PATH, 'w'))
