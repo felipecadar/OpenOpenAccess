b61cde661daf41fe5beec97f423258361874fd58
Felipe Cadar <cadar@dcc.ufmg.br>
2024-05-17 17:58:49 +0200

    speed analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import "~/styles/globals.css";
+
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights/>
     </>
   );
 };
